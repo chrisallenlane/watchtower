08e0bf7c2ca1046f451ec58c55a03fa2cd9b7008
antisnatchor <antisnatchor@gmail.com>
2012-07-23 17:59:02 +0100

    Added Java basic checks

--- config.rb
+++ config.rb
@@ -72,8 +72,9 @@ $configs = {
 		#'./signatures/c.rb',
 		'./signatures/html.rb',
 		'./signatures/asp_net.rb',
+    './signatures/java.rb',
     './signatures/js.rb',
-		# './signatures/sql.rb',
+		'./signatures/sql.rb',
 		# './signatures/wordpress.rb',
 		# './signatures/blocklist.rb',
 	].sort,
@@ -129,7 +130,7 @@ $configs[:ftype_ext][:scala]         = %w[scala]
 $configs[:ftype_ext][:scheme]        = %w[scm ss]
 $configs[:ftype_ext][:shell]         = %w[sh bash csh tcsh ksh zsh]
 $configs[:ftype_ext][:smalltalk]     = %w[st]
-$configs[:ftype_ext][:sql]           = %w[sql ctl]
+$configs[:ftype_ext][:sql]           = %w[sql ctl java groovy asp aspx cs]
 $configs[:ftype_ext][:tcl]           = %w[tcl itcl itk]
 $configs[:ftype_ext][:tex]           = %w[tex cls sty]
 $configs[:ftype_ext][:tt]            = %w[tt tt2 ttml]
@@ -142,6 +143,7 @@ $configs[:ftype_ext][:wordpress]     = %w[php phpt php3 php4 php5 phtml]
 
 # map the ASP.NET signature to various files, includes also .cs files (C#)
 $configs[:ftype_ext][:asp_net]     = %w[asp aspx cs]
+$configs[:ftype_ext][:java]     = %w[java jsp groovy properties xml]
 
 # map the blocklists to some filetypes (defaulting to PHP and HTML)
 # Note: if you're going to scan blocklists, be prepared to wait a 
---
+++ signatures/java.rb
@@ -0,0 +1,43 @@
+$signatures[:java] ||= {}
+$signatures[:java][:file_access] = [
+    # File access
+    Signature.new({:literal => 'new FileInputStream'}),
+    Signature.new({:literal => 'new FileOutputStream'}),
+    Signature.new({:literal => 'new FileReader'}),
+    Signature.new({:literal => 'new FileWriter'}),
+    Signature.new({:literal => 'new File'}),
+]
+
+$signatures[:java][:db_access] = [
+    # DB access
+    # no PreparedStatement -> potentially vulnerable
+    Signature.new({:literal => 'createStatement'}),
+    Signature.new({:literal => '.execute'}),
+    Signature.new({:literal => '.executeQuery'}),
+    Signature.new({:literal => 'Statement.execute'}),
+    Signature.new({:literal => 'Statement.executeQuery'}),
+]
+
+$signatures[:java][:external_process] = [
+    # External Process
+    Signature.new({:literal => 'Runtime.getRuntime'}),
+    Signature.new({:literal => 'Runtime.Exec'}),
+    Signature.new({:literal => 'Process'}),
+]
+
+
+$signatures[:java][:HTTP_request] = [
+    # HTTP redirect/playing with response headers
+    Signature.new({:literal => '.sendRedirect'}),
+    Signature.new({:literal => '.setStatus'}),
+    Signature.new({:literal => '.addHeader'}),
+    Signature.new({:literal => '.getParameter'}),
+    Signature.new({:literal => '.getHeader'}),
+]
+
+$signatures[:java][:Crypto_and_Keystore]= [
+    # Keystore/Crypto usage
+    Signature.new({:literal => 'KeyStore.'}),
+    Signature.new({:literal => 'PrivateKey'}),
+    Signature.new({:literal => 'SamlAuthToken'}),
+]
